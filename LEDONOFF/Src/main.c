7c673e4ea0d2131c582d2b141f93c35d3d93b42a
obs <v4dd1@naver.com>
2019-04-30 11:21:07 +0900

    스위치 기능 넣음

--- LEDONOFF/Src/main.c
+++ LEDONOFF/Src/main.c
@@ -99,14 +99,26 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  int dleay_value = 0;
+
   while (1)
   {
+	  if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_0)==0);
+	  {
+		  dleay_value = 500;
+	  }
+
+	  if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_1)==0);
+	  {
+		  dleay_value =  300;
+	  }
+
 	  HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_13);
-	  HAL_Delay(500);
+	 // HAL_Delay(300);
 
-	  /* USER CODE END WHILE */
+  /* USER CODE END WHILE */
 
-	  /* USER CODE BEGIN 3 */
+  /* USER CODE BEGIN 3 */
 
   }
   /* USER CODE END 3 */
@@ -178,6 +190,7 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
@@ -189,6 +202,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB0 PB1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
